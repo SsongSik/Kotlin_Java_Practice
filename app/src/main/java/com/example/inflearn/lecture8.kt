515d669bfd7f8f916e597cf1242bcc28dfe76246
sgsk88 <sgsk88@nate.com>
2022-08-05 15:11:16 +0900

    custom getter, setter

--- app/src/main/java/com/example/inflearn/lecture8.kt
+++ app/src/main/java/com/example/inflearn/lecture8.kt
@@ -28,16 +28,37 @@ class Person constructor(name : String, age : Int){
 //이렇게 바꿔줄 수 있음
 //프로퍼티 선언과 생성자를 동시에 합칠 수 있음
 class Person1 (
-    val name : String, var age : Int
+    name : String, var age : Int
 ){
+    /*
+    원래 name은 getter를 포함하고 있었기 때문에 밑에 커스텀 getter를 만들어주기위해
+    밑에다가 선언
+    name에 대한 custom getter를 사용할 때 field를 사용
+    안에서 name을 했을 때 호출하게 되면 getter를 호출하게 되는데 우리는 밑에 getter를 선언함
+    따라서 계속 getter를 호출하게 돼서 무한루프가 발생함
+    -> 자기 자신을 가리키는 field를 사용
+    val name = name
+     get() = field.uppercase()
+
+     하지만 밑과 같이 이렇게도 표현이 가능하여 backing field를 사용하는 일은 거의 없음
+     fun getUpper() : String{
+        return this.name.uppercase()
+    }
+
+    val upperName : String
+     get() = this.name.uppercase()
+     */
+
+    var name = name
+     set(value){
+         field = value.uppercase()
+     }
+
     init{ //클래스가 초기화되는 시점에서 한 번 실행되는 지점, 생성자가 실행되는 시점
         if(age <= 0){
             throw IllegalArgumentException("나이는 ${age}일 수 없습니다.")
         }
         println("초기화") //이는 역순으로 실행됨. = 초기화 블록부터 차례로 호출된다.
-    }
-    constructor(name : String): this(name, 1){
-        println("첫 번째 부 생성자")
     }
     //생성자를 추가하고 싶은 경우에는 위의 생성자를 그대로 두고
     //아래 생성자를 새로 적어줌.
@@ -46,5 +67,22 @@ class Person1 (
     아래 있는 생성자는 부 생성자로 부르고, 없을 수도 있다.
     또한, 최종적으로 주 생성자를 this로 호출해야한다.
     body를 가질 수 있다.
+    하지만 부 생성자 보다는 default파라메타를 넣어서 하는 것을 권장하고 있다.
      */
+    constructor(name : String): this(name, 1){
+        println("첫 번째 부 생성자")
+    }
+
+    fun isAdult() : Boolean{
+        return this.age >= 20
+    }
+
+    //밑에 두개는 커스텀 getter
+    val isAdult1 : Boolean
+    get() {
+        return this.age >= 20
+    }
+
+    val isAdult2 : Boolean
+     get() = this.age >= 20
 }
