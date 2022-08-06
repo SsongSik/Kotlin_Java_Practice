bd64624a254b0dde5a3fe13fb03155704a6c9bba
sgsk88 <sgsk88@nate.com>
2022-08-06 16:06:15 +0900

    lecture 1~8

--- app/src/main/java/com/example/inflearn/lecture9.kt
+++ app/src/main/java/com/example/inflearn/lecture9.kt
@@ -1,5 +1,17 @@
 package com.example.inflearn
 
 fun main(){
+    val numbers = listOf(1, 2, 3)
 
+    for(number in numbers){
+        println(number)
+    }
+
+    for(i in 1..3){
+        println(i)
+    }
+
+    for(i in 100 downTo 1 step 2){
+        println(i)
+    }
 }
